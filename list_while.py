5c702b3459b9d90d3604c4fd9e35a9e37d1d893e
asilbek-data <asilprogrammer@gmail.com>
2026-03-11 13:26:08 +0500

    Create list_while.py

---
+++ list_while.py
@@ -0,0 +1,28 @@
+"""
+11/2/2026
+"""
+
+# savat =[]
+# while True:
+#     mahsulot = input("Savatga mahsulot qo'shing:")
+#     savat.append(mahsulot)
+#     javob = input("Yana mahsulot qo\'shasizmi?(ha/yo\'q)")
+#     if javob != 'ha':
+#         break
+
+
+# buyurtmalar = ['olma','anjir','uzum','qovun']
+# mahsulotlar = {'olma':20000,
+#                'shaftoli':25000,
+#                'tarvuz':18000,
+#                'uzum':22000}
+
+# while buyurtmalar:
+#     buyurtma = buyurtmalar.pop()
+#     if buyurtma in mahsulotlar.keys():
+#         narh = mahsulotlar[buyurtma]
+#         print(f"{buyurtma.title()} - {narh} so'm")
+#     else:
+#         print(f"Bizda {buyurtma} yo'q")
+
+
